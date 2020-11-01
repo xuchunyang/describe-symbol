79e43d11ed981193c77ff17e5f656be5ebf304bf
Chunyang Xu <mail@xuchunyang.me>
2020-11-01 21:28:10 +0800

    * json-to-harperdb.js: Avoid replaceAll which not work in node 12

--- json-to-harperdb.js
+++ json-to-harperdb.js
@@ -6,7 +6,7 @@ const jsonFile = process.argv[2];
 const json = JSON.parse(fs.readFileSync(jsonFile, "utf-8"));
 const emacsVersion = json["emacs-version"];
 // Avoid . in the table name because of select * from dev.dog
-const tableName = "emacs_" + emacsVersion.replaceAll(".", "_");
+const tableName = "emacs_" + emacsVersion.replace(/\./g, "_");
 const records = json.data;
 
 const DB_CONFIG = {
